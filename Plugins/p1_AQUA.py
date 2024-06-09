cd47d850aaf348e8765df5fe9e9c2ad809cdbedf
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-06-09 15:23:51 +0800

    Add files via upload

--- Plugins/p1_AQUA.py
+++ Plugins/p1_AQUA.py
@@ -1,4 +1,6 @@
 def log_process(clearT, Cfg, Windows):
     var_d: str = Cfg.GetClipboardText()[::2]
     var_n = Cfg.var_n if var_d.startswith('「') else '旁白'
+    if var_d.startswith('（') and var_d.endswith('）'):
+        var_n = Cfg.var_n
     return clearT(var_n) + '：' + clearT(var_d)
