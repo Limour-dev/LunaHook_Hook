b517023b3377c0221441957a0426b1ff4752a6ec
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-06-14 19:08:45 +0800

    Add files via upload

---
+++ Plugins/p3_仰望夜空星辰.py
@@ -0,0 +1,28 @@
+def startsWithAny(s: str, keys):
+    for x in keys:
+        if s.startswith(x):
+            return x
+    else:
+        return False
+
+
+def endsWithAny(s: str, keys):
+    for x in keys:
+        if s.endswith(x):
+            return x
+    else:
+        return False
+
+
+brackets = {'「': '」', '（': '）', '『': '』', '【': '】', '“': '”'}
+
+
+def log_process(clearT, Cfg, Windows):
+    var_d: str = Cfg.var_d
+    var_d = var_d.strip()
+    tmp = startsWithAny(var_d, brackets.keys())
+    if tmp and var_d.endswith(brackets[tmp]):
+        var_n = Cfg.var_n
+    else:
+        var_n = '旁白'
+    return clearT(var_n) + '：' + clearT(var_d)
