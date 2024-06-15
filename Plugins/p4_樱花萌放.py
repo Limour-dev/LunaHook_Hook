b54596bc094401e252e16f93795aaa16c66a7340
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-06-15 15:07:10 +0800

    Add files via upload

--- Plugins/p4_樱花萌放.py
+++ Plugins/p4_樱花萌放.py
@@ -21,6 +21,14 @@ def endsWithAny(s: str, keys):
         return False
 
 
+def getPrevLine(Cfg, var_d):
+    tmp: str = Cfg.oldText
+    idx_r = tmp.rindex(var_d) - 2
+    idx_l = tmp.rindex('\r\n', 0, idx_r)
+    var_n = tmp[idx_l + 2:idx_r]
+    return var_n
+
+
 def log_process(clearT, Cfg, Windows):
     global oldLine
     var_d: str = Cfg.GetEditText()
@@ -29,19 +37,27 @@ def log_process(clearT, Cfg, Windows):
     tmp = endsWithAny(var_d, brackets_r.keys())
     if not tmp:
         var_n = '旁白'
+        if '「' in var_d and '」' not in var_d:
+            return ''
+        if '『' in var_d and '』' not in var_d:
+            return ''
     else:
         if var_d.startswith(brackets_r[tmp]):
-            tmp: str = Cfg.oldText
-            idx_r = tmp.rindex(var_d) - 2
-            idx_l = tmp.rindex('\r\n', 0, idx_r)
-            var_n = tmp[idx_l + 2:idx_r]
+            var_n = getPrevLine(Cfg, var_d)
         else:
-            idx = var_d.index(brackets_r[tmp])
+            if brackets_r[tmp] in var_d:
+                idx = var_d.index(brackets_r[tmp])
+            else:
+                var_d += (getPrevLine(Cfg, var_d) + '\r\n')
+                idx = var_d.index(brackets_r[tmp])
             if idx > 5:
                 var_n = '旁白'
+            elif idx == 0:
+                var_n = getPrevLine(Cfg, var_d)
             else:
                 var_n = var_d[:idx]
                 var_d = var_d[idx:]
+
     var_d = var_d.replace('\n', '').replace('\r', '')
     if not var_d.startswith('「') and oldLine and var_d != oldLine and oldLine.endswith(var_d):
         return ''
