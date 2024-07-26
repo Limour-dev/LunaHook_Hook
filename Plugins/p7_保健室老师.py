6c2507f747b36e3d68b4d5a4ff691d4209059d0c
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-07-26 15:14:22 +0800

    Add files via upload

---
+++ Plugins/p7_保健室老师.py
@@ -0,0 +1,75 @@
+def startsWithAny(s: str, keys):
+    for x in keys:
+        if s.startswith(x):
+            return x
+    else:
+        return False
+
+
+def indexWithAny(s: str, keys):
+    for x in keys:
+        if x in s:
+            idx = s.index(x)
+            return x, s[:idx], s[idx:]
+    else:
+        return False
+
+
+def endsWithAny(s: str, keys):
+    for x in keys:
+        if s.endswith(x):
+            return x
+    else:
+        return False
+
+
+brackets = {'「': '」', '『': '』', '（': '）', '【': '】', '“': '”'}
+brackets_r = {v: k for k, v in brackets.items()}
+
+cache = []
+cache_tmp = ''
+cache_var_n = ''
+
+
+def log_process(clearT, Cfg, Windows):
+    var_d: str = Cfg.var_d
+    tmp = endsWithAny(var_d, brackets_r.keys())
+    tmp2 = ''
+    if tmp == '）' and '（' in var_d:
+        _var_d = var_d[:var_d.rindex('（')].rstrip()
+        tmp2 = endsWithAny(_var_d, brackets_r.keys())
+        if tmp2:
+            tmp = tmp2
+            tmp2 = var_d[var_d.rindex('（'):]
+            var_d = _var_d
+        else:
+            tmp2 = ''
+
+    global cache, cache_tmp, cache_var_n
+    if not tmp:
+        if cache:
+            cache.append(var_d)
+            return ''
+        tmp = indexWithAny(var_d, brackets.keys())
+        if not tmp or brackets[tmp[0]] in var_d:
+            return '旁白：' + clearT(var_d)
+        else:
+            cache_var_n = tmp[1]
+            cache_tmp = tmp[0]
+            cache.append(tmp[2])
+            return ''
+    else:
+        if cache:
+            var_n = cache_var_n
+            cache.append(var_d)
+            var_d = ' '.join(cache)
+            cache = []
+            return clearT(var_n) + ('：' if brackets_r[tmp] == cache_tmp else '<sp>：') + clearT(var_d + tmp2)
+
+        elif brackets_r[tmp] in var_d:
+            idx = var_d.index(brackets_r[tmp])
+            var_n = var_d[:idx]
+            var_d = var_d[idx:]
+            return clearT(var_n) + '：' + clearT(var_d + tmp2)
+        else:
+            return '旁白<sp>：' + clearT(var_d + tmp2)  # 标记一下，后期手动处理
